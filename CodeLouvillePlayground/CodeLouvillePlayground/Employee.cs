7bdd78a867698b530fe8fb72ba7af4d1baff72fc
mranthony101 <mranthony101@gmail.com>
2022-05-31 18:10:59 -0600

    Add files via upload

---
+++ CodeLouvillePlayground/CodeLouvillePlayground/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLouvillePlayground
+{
+    public class Employee
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string FullName { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        private int _salary { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string FirstName, string LName, string Title, DateTime Start, int Salary, int Age)
+        {
+            this.FName = FirstName;
+            this.LName = LName;
+            this.FullName = $"{this.FName} {this.LName}";
+            this.Start = Start;
+            this.Title = Title;
+            this._salary = Salary;
+            this.Age = Age;
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine($"{this.FullName} and I started on {this.Start}");
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine("I am working");
+        }
+
+        public void DoWork(string workingTask)
+        {
+            Console.WriteLine($"I am working on {workingTask}");
+        }
+
+        public void ShowTitle()
+        {
+            Console.WriteLine($"My Title is {this.Title}");
+        }
+
+        public void ShowPay()
+        {
+            Console.WriteLine($"My pay is: {_salary}");
+        }
+
+    }
+
+
+}
---
+++ CodeLouvillePlayground/CodeLouvillePlayground/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeLouvillePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter in a number.");
+                var t = int.Parse(Console.ReadLine());
+            }
+            catch(Exception)
+            {
+                Console.WriteLine($"An exception was thrown");
+            }
+
+
+
+
+
+            Employee employee1 = new Employee("Ross", "Anthony", "Mentor", DateTime.Now, 0, 30);
+            Employee employee2 = new Employee("Trent", "M", "Boss", Convert.ToDateTime("5/31/2021"), 10, 30);
+
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(employee1);
+            employeeList.Add(employee2);
+
+            foreach(Employee tempEmployeeObj in employeeList)
+            {
+                tempEmployeeObj.ShowName();
+                tempEmployeeObj.ShowTitle();
+                tempEmployeeObj.ShowPay();
+            }
+        }      
+    }
+}
